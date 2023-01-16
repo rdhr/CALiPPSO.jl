dda5823030a9f18f946d02ef5cd7f46d620482f0
Rafael Diaz <rafael.diaz.hr@gmail.com>
2023-01-16 17:16:50 +0100

    changing solver examples compatible with V0.2.2

--- Examples/testing-different-solvers.jl
+++ Examples/testing-different-solvers.jl
@@ -16,8 +16,9 @@ const default_tol_displacements = CALiPPSO.default_tol_displacements_convergence
 const tol_optimality = CALiPPSO.default_tol_optimality
 const max_threads = CALiPPSO.max_threads
 
-const solvers = [:Gurobi, :HiGHS, :Clp, :GLPK, :COSMO] # array of solver's names (or symbols)
-using Gurobi, HiGHS, Clp, GLPK, COSMO #,Hypatia
+const solvers = [:Gurobi, :HiGHS, :GLPK, :Clp] # array of solver's names (or symbols)
+using Gurobi, HiGHS, Clp #COSMO #,Hypatia
+using CALiPPSO.GLPK
 
 
 println("This script will test the following solvers: ",  map(x->x*", ", string.(solvers))...)
@@ -28,38 +29,39 @@ println("But before the test is executed, `produce_jammed_configuration!` will b
 ##############################
 
 # Gurobi:
-const grb_args = Gurobi.Env()
+const grb_env = Gurobi.Env()
+const grb_opt = Gurobi.Optimizer(grb_env)
 const grb_attributes = Dict("OutputFlag" => 0, "FeasibilityTol" => tol_optimality, "OptimalityTol" => tol_optimality, "Method" => 3, "Threads" => max_threads)
-precompile_main_function(Gurobi, grb_attributes, grb_args)
+precompile_main_function(grb_opt, grb_attributes)
 
 
 # HiGHS
-const highs_args = nothing
+const highs_opt = HiGHS.Optimizer()
 const highs_attributes = Dict("small_matrix_value"=>0.1*tol_optimality, "primal_feasibility_tolerance" => tol_optimality, "dual_feasibility_tolerance" => tol_optimality, "solver" => "ipm", "ipm_optimality_tolerance"=>tol_optimality,  "threads" => max_threads, "parallel"=>"on", "output_flag"=>false)
 printstyled("\n______________________________________________________________________________________\n", color=:yellow)
-precompile_main_function(HiGHS, highs_attributes, highs_args)
-
-
-
-# Clp
-const clp_args = nothing
-const clp_attributes = Dict("PrimalTolerance"=>tol_optimality, "DualTolerance" => tol_optimality, "LogLevel" => 0, "SolveType" => 5)
-printstyled("\n______________________________________________________________________________________\n", color=:yellow)
-precompile_main_function(Clp, clp_attributes, clp_args)
+precompile_main_function(highs_opt  , highs_attributes)
 
 
 # GLPK
-const glpk_args = (want_infeasibility_certificates=false, method=GLPK.MethodEnum(0) ) 
+const glpk_opt = GLPK.Optimizer(want_infeasibility_certificates=false, method=GLPK.MethodEnum(0))
 const glpk_attributes = Dict("msg_lev"=>GLPK.GLP_MSG_OFF, "tol_bnd"=>tol_optimality, "tol_dj"=>tol_optimality)
 printstyled("\n______________________________________________________________________________________\n", color=:yellow)
-precompile_main_function()
+precompile_main_function(glpk_opt, glpk_attributes)
 
 
-# COSMO
-const cosmo_args = nothing
-const cosmo_attributes = Dict("verbose"=>false, "max_iter"=>30000, "eps_abs"=>1e3*tol_optimality, "adaptive_rho"=>false, "eps_prim_inf"=>1e3*tol_optimality, "eps_dual_inf"=>1e3tol_optimality)
+# Clp
+const clp_opt = Clp.Optimizer()
+const clp_attributes = Dict("PrimalTolerance"=>tol_optimality, "DualTolerance" => tol_optimality, "LogLevel" => 0, "SolveType" => 5)
 printstyled("\n______________________________________________________________________________________\n", color=:yellow)
-precompile_main_function(COSMO, cosmo_attributes, cosmo_args, true)
+precompile_main_function(clp_opt, clp_attributes)
+
+
+# These solvers are not used due to accuracy issues
+# # COSMO
+# const cosmo_opt = COSMO.Optimizer()
+# const cosmo_attributes = Dict("verbose"=>false, "max_iter"=>30000, "eps_abs"=>1e3*tol_optimality, "adaptive_rho"=>false, "eps_prim_inf"=>1e3*tol_optimality, "eps_dual_inf"=>1e3tol_optimality)
+# printstyled("\n______________________________________________________________________________________\n", color=:yellow)
+# precompile_main_function(cosmo_opt, cosmo_attributes)
 
 
 # # Hypatia
@@ -73,8 +75,9 @@ precompile_main_function(COSMO, cosmo_attributes, cosmo_args, true)
 
 
 #### create arrays of these variables, for easier calling
-const solvers_args = [grb_args, highs_args, clp_args, glpk_args, cosmo_args]
-const solvers_attributes = [grb_attributes, highs_attributes,clp_attributes ,glpk_attributes, cosmo_attributes]
+# const solvers_args = [grb_args, highs_args, clp_args, glpk_args, cosmo_args]
+const optimizers = [grb_opt, highs_opt, glpk_opt, clp_opt]
+const solvers_attributes = [grb_attributes, highs_attributes, glpk_attributes, clp_attributes]
 
 
 ## Array of packings to perform later comparison
@@ -91,28 +94,36 @@ printstyled("\n\n O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O 
 printstyled("Applying CALiPPSO for jamming a system of N = ", N, " in d = ", d, "\t Initial φ = ", packing_fraction(d, r0_common, N, L), "\n", bold=:true, color=:blue)
 printstyled("O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O --- O\n", color=:blue, bold=true)
 
-for (ns, solver) in enumerate(solvers)
+# for (ns, solver) in enumerate(solvers)
+for (ns, optimizer) in enumerate(optimizers)
     Xs0 = 1.0.*Xs_common; r0=1*r0_common
-    
-    if  Symbol(solver)==:COSMO 
-        # continue
-        overlap_tolerance = 1e3*tol_overlap
-        zero_force = 1e-5
-        Γ_conv = tol_Γ_convergence
-        add_bridges = true
-    elseif Symbol(solver)==:Hypatia
-        # continue
-        overlap_tolerance = tol_overlap
-        zero_force = 1e-5
-        Γ_conv = 1e-10
-        add_bridges = false
-    else
-        # continue
-        overlap_tolerance = tol_overlap
-        zero_force = tol_zero_forces
-        Γ_conv = tol_Γ_convergence
-        add_bridges = false
-    end
+
+    overlap_tolerance = tol_overlap
+    zero_force = tol_zero_forces
+    Γ_conv = tol_Γ_convergence
+    add_bridges = false
+
+    solver_attrs = solvers_attributes[ns]
+    solver = solvers[ns]
+    # if  Symbol(solver)==:COSMO 
+    #     # continue
+    #     overlap_tolerance = 1e3*tol_overlap
+    #     zero_force = 1e-5
+    #     Γ_conv = tol_Γ_convergence
+    #     add_bridges = true
+    # elseif Symbol(solver)==:Hypatia
+    #     # continue
+    #     overlap_tolerance = tol_overlap
+    #     zero_force = 1e-5
+    #     Γ_conv = 1e-10
+    #     add_bridges = false
+    # else
+    #     # continue
+    #     overlap_tolerance = tol_overlap
+    #     zero_force = tol_zero_forces
+    #     Γ_conv = tol_Γ_convergence
+    #     add_bridges = false
+    # end
     S_conv = default_tol_displacements
 
     printstyled("\n\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", color=:magenta, bold=true)
@@ -120,10 +131,10 @@ for (ns, solver) in enumerate(solvers)
     println("\tConvergence criteria: tolerance √Γ-1 = ",Γ_conv, "\t tolerance |sᵢ| = ", S_conv)
     printstyled("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n", color=:magenta, bold=true)
 
-    solver_args = solvers_args[ns]; solver_attrs = solvers_attributes[ns]
+
 
     @time jammed_packing, info_convergence, Γs_vs_t, smax_vs_t, iso_vs_t = produce_jammed_configuration!(Xs0, r0; verbose=true, 
-    solver=eval(solver), solver_args=solver_args, solver_attributes=solver_attrs, tol_Γ_convergence=Γ_conv, tol_S_convergence = S_conv,
+    optimizer=optimizer, solver_attributes=solver_attrs, tol_Γ_convergence=Γ_conv, tol_S_convergence = S_conv,
     tol_overlap=overlap_tolerance, initial_monitor=30, zero_force=zero_force, max_iters=25, add_bridges=add_bridges) 
 
     println("_______________________________________________________________________________________\n\n")
